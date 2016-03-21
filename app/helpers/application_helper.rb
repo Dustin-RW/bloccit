fe5263d2f9368595a4eb45259e5bf51a69443b08
Dustin <dustinwaggoner@comcast.net>
2016-03-20 22:19:05 -0700

    added notes from checkpoint-35

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
 
-  # helper method to display errors in app.  See post partial
+  # helper method to display errors in app.  See post partial (_form) for example
   def form_group_tag(errors, &block)
     css_class = 'form-group'
     css_class << ' has-error' if errors.any?
