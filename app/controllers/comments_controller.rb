b4bd8cb9d91ba3776d64576a9d7e320e4977516b
Dustin <dustinwaggoner@comcast.net>
2016-03-26 16:31:26 -0700

    still working on comment create action take a Topic comment or a Post comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,55 +1,60 @@
 class CommentsController < ApplicationController
-
-  #found in ApplicationController.  Unless user is signed in, they cannot
-  #perform any actions within the CommentsController and will receive an error
+  # found in ApplicationController.  Unless user is signed in, they cannot
+  # perform any actions within the CommentsController and will receive an error
   before_action :require_sign_in
-  #unauthorized users are not allowed to delete comments
+  # unauthorized users are not allowed to delete comments
   before_action :authorize_user, only: [:destroy]
 
   def create
-    @topic = Topic.find(params[:topic_id])
-    topic_comment = @topic.comment.new(comment_params)
-    #find the correct post using the :post_id from params
-    @post = Post.find(params[:post_id])
-    #create a new comment using comment_params
-    post_comment = @post.comments.new(comment_params)
-    #assign the comments user to current_user which returns the signedin user instance
-    comment.user = current_user
 
-    if post_comment.save
-      flash[:notice] = "Comment saved successfully"
+    id = params[:post_id] || params[:topic_id]
+    if params[:post_id]
+      @parent = Post.find(id)
+    elsif params[:topic_id]
+      @parent = Topic.find(id)
+    end
+    p "Here is the passed id", @parent.attributes
+    p "here is a comment?", @parent.comments
+    @comment = @parent.comments.find_by(params[:id])
+    p "Here is the new comment", @comment.attributes
+
+    @comment.save
+
+
+=begin
+    if @parent == params[:post_id]
+      comment.save
+      flash[:notice] = 'Comment saved successfully'
       redirect_to [@post.topic, @post]
-    elsif topic_comment.save
-      flash[:notice] = "Comment saved successfully"
+    elsif @parent.topic == params[:topic_id]
+      comment.save
+      flash[:notice] = 'Comment saved successfully'
       redirect_to @topic
     else
-      flash[:alert] = "Comment failed to save"
-      redirect_to [@post.topic, @post]
+      flash[:alert] = 'Comment failed to save'
+      redirect_to :back
     end
-
+=end
   end
 
   def destroy
-
     comment = Comment.find(params[:id])
-    #@topic = Topic.find(params[:topic_id])
-    #topic_comment = @topic.comments.find(params[:id])
+    # @topic = Topic.find(params[:topic_id])
+    # topic_comment = @topic.comments.find(params[:id])
 
-    #@post = Post.find(params[:post_id])
-    #post_comment = @post.comments.find(params[:id])
+    # @post = Post.find(params[:post_id])
+    # post_comment = @post.comments.find(params[:id])
 
     if comment.destroy
-      flash[:notice] = "Comment was deleted"
+      flash[:notice] = 'Comment was deleted'
       redirect_to :back
     else
       flash[:alert] = "Comment counld't be deleted. Try again"
       redirect_to :back
     end
-
   end
 
   private
-
 
   def comment_params
     params.require(:comment).permit(:body)
@@ -59,9 +64,8 @@ class CommentsController < ApplicationController
     comment = Comment.find(params[:id])
 
     unless current_user == comment.user || current_user.admin?
-      flash[:alert] = "You do not have permission to delete a comment."
+      flash[:alert] = 'You do not have permission to delete a comment.'
       redirect_to [comment.post.topic, comment.post]
     end
   end
-
 end
