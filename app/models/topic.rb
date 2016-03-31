a3125f894894431fd166c382e64de005be8f3610
Dustin <dustinwaggoner@comcast.net>
2016-03-31 09:26:43 -0700

    still working on checkpoint

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -5,4 +5,17 @@ class Topic < ActiveRecord::Base
   #define a has many relationship between Topic and a label using the Label class through the
   #labelable interface
   has_many :labels, through: :labelings
+
+  # We use lambda syntax (the arrow -> with a block), which is the
+  # proper way to pass code into a scope definition. This scope runs the
+  # code inside the lambda on the relation or class on which it's called.
+  # In other words, Topic.visible_to is equivalent to Topic.where(public: true)
+  # scope :visible_to, -> { where(public: true) }
+
+  # instead of above, where we would have to implement a if statement
+  # within TopicController, we instead pass the scope an argument to
+  # determine if a user is signed in or not
+  scope :visible_to, -> (user) { user ? all : where(public: true) }
+
+
 end
