182a38ce0f37fe1e199ba3de17e1c60288799bba
Dustin <dustinwaggoner@comcast.net>
2016-04-01 14:18:49 -0700

    checkpoint-47 complete

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,45 +1,55 @@
 class CommentsController < ApplicationController
-
-  #found in ApplicationController.  Unless user is signed in, they cannot
-  #perform any actions within the CommentsController and will receive an error
+  # found in ApplicationController.  Unless user is signed in, they cannot
+  # perform any actions within the CommentsController and will receive an error
   before_action :require_sign_in
-  #unauthorized users are not allowed to delete comments
+  # unauthorized users are not allowed to delete comments
   before_action :authorize_user, only: [:destroy]
 
   def create
-    #find the correct post using the :post_id from params
+    # find the correct post using the :post_id from params
     @post = Post.find(params[:post_id])
-    #create a new comment using comment_params
-    comment = @post.comments.new(comment_params)
-    #assign the comments user to current_user which returns the signedin user instance
-    comment.user = current_user
+    # create a new comment using comment_params
+    @comment = @post.comments.new(comment_params)
+    # assign the comments user to current_user which returns the signedin user instance
+    @comment.user = current_user
 
-    if comment.save
-      flash[:notice] = "Comment saved successfully"
-      redirect_to [@post.topic, @post]
+    @new_comment = Comment.new
+
+    if @comment.save
+      flash[:notice] = 'Comment saved successfully'
     else
-      flash[:alert] = "Comment failed to save"
-      redirect_to [@post.topic, @post]
+      flash[:alert] = 'Comment failed to save'
     end
 
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   def destroy
-
     @post = Post.find(params[:post_id])
-    comment = @post.comments.find(params[:id])
+    # replaced comment with @comment because we'll need to have access to the
+    # variable in our .js.erb view
+    @comment = @post.comments.find(params[:id])
 
-    if comment.destroy
-      flash[:notice] = "Comment was deleted"
-      redirect_to [@post.topic, @post]
+    if @comment.destroy
+      flash[:notice] = 'Comment was deleted'
     else
       flash[:alert] = "Comment counld't be deleted. Try again"
-      redirect_to [@post.topic, @post]
+    end
+
+    # respond_to block gives our controller action the ability to return
+    # different response types, depending on what was asked for in the request.
+    # The controller's response is unchanged if the client requests HTML, but
+    # if the client requests JavaScript, the controller will render .js.erb instead
+    respond_to do |format|
+      format.html
+      format.js
     end
   end
 
   private
-
 
   def comment_params
     params.require(:comment).permit(:body)
@@ -49,9 +59,8 @@ class CommentsController < ApplicationController
     comment = Comment.find(params[:id])
 
     unless current_user == comment.user || current_user.admin?
-      flash[:alert] = "You do not have permission to delete a comment."
+      flash[:alert] = 'You do not have permission to delete a comment.'
       redirect_to [comment.post.topic, comment.post]
     end
   end
-
 end
--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -19,7 +19,7 @@ RSpec.describe CommentsController, type: :controller do
   context 'guest' do
     describe 'POST create' do
       it 'redirects the user to the sign-in view' do
-        post :create, post_id: my_post.id, comment: { body: RandomData.random_paragraph }
+        post :create, format: :js, post_id: my_post.id, comment: { body: RandomData.random_paragraph }
 
         expect(response).to redirect_to(new_session_path)
       end
@@ -44,12 +44,12 @@ RSpec.describe CommentsController, type: :controller do
 
     describe 'POST create' do
       it 'increases the number of comments by 1' do
-        expect { post :create, post_id: my_post.id, comment: { body: RandomData.random_sentence } }.to change(Comment, :count).by(1)
+        expect { post :create, format: :js, post_id: my_post.id, comment: { body: RandomData.random_sentence } }.to change(Comment, :count).by(1)
       end
 
-      it 'redirects to the post show view' do
-        post :create, post_id: my_post.id, comment: { body: RandomData.random_sentence }
-        expect(response).to redirect_to [my_topic, my_post]
+      it 'returns http success' do
+        post :create, format: :js, post_id: my_post.id, comment: { body: RandomData.random_sentence }
+        expect(response).to have_http_status(:success)
       end
     end
 
@@ -70,13 +70,13 @@ RSpec.describe CommentsController, type: :controller do
 
     describe 'POST create' do
       it 'increases the number of comments by 1' do
-        expect { post :create, post_id: my_post.id, comment: { body: RandomData.random_sentence } }.to change(Comment, :count).by(1)
+        expect { post :create, format: :js, post_id: my_post.id, comment: { body: RandomData.random_sentence } }.to change(Comment, :count).by(1)
       end
 
-      it 'redirects to the post show view' do
-        post :create, post_id: my_post.id, comment: { body: RandomData.random_sentence }
+      it 'returns http success' do
+        post :create, format: :js, post_id: my_post.id, comment: { body: RandomData.random_sentence }
 
-        expect(response).to redirect_to [my_topic, my_post] # same as topic_post_path (/topics/:topic_id/posts/:id) route
+        expect(response).to have_http_status(:success) # same as topic_post_path (/topics/:topic_id/posts/:id) route
       end
     end
 
@@ -107,12 +107,12 @@ RSpec.describe CommentsController, type: :controller do
 
     describe 'POST create' do
       it 'increases the number of comments by 1' do
-        expect { post :create, post_id: my_post.id, comment: { body: RandomData.random_sentence } }.to change(Comment, :count).by(1)
+        expect { post :create, format: :js, post_id: my_post.id, comment: { body: RandomData.random_sentence } }.to change(Comment, :count).by(1)
       end
 
-      it 'redirects to the post show view' do
-        post :create, post_id: my_post.id, comment: { body: RandomData.random_sentence }
-        expect(response).to redirect_to [my_topic, my_post]
+      it 'returns http success' do
+        post :create, format: :js, post_id: my_post.id, comment: { body: RandomData.random_sentence }
+        expect(response).to have_http_status(:success)
       end
     end
 
